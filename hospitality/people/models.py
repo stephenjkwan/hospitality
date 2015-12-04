dd7797b726065e2326d28ccb7d8469d4fcdfd9a8
Stephen Kwan <stephenjkwan@gmail.com>
2015-12-03 23:43:07 -0500

    still working on models

--- hospitality/people/models.py
+++ hospitality/people/models.py
@@ -1,9 +1,18 @@
 from django.db import models
-
+#SQL databse will have one table for Person
 class Person(models.Model):
 	first_name = models.CharField(max_length=30)
 	last_name = models.CharField(max_length=30)
-	class Car(models.Model):
+	
+#SQL will have another database for Cars but cars will have another field
+# for a foreign key which references the person in the Person database
+class Car(models.Model):
+	owner = models.ForeignKey(Person)
+	avaliable = models.BooleanField(initial=False)
+	def canUseCar(self):
+		self.avaliable = True
+	
+
 		#consdering using a foreign key not sure what its for
 		#I want the cars to relate to the person
 
